29d4c7c1f43c6f2d57de7e4ada3843308129bc67
ZSY <ZSY@.com>
2018-05-05 10:16:13 +0800

    发布修改

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -18,8 +18,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
   },
   // cheap-module-eval-source-map is faster for development
-  devtool: config.dev.devtool,
-
+  // devtool: config.dev.devtool,
+  devtool: false,
   // these devServer options should be customized in /config/index.js
   devServer: {
     clientLogLevel: 'warning',
