e2c3f89fc306c57203eddd2604c8acce2a20148f
ZSY <ZSY@.com>
2018-05-05 18:00:10 +0800

    添加图标

--- src/components/SwipeUpIcon.vue
+++ src/components/SwipeUpIcon.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-
+    <div class="swipe-up-icon-box">
+      <div class="swipe-up-icon">
+        <i class="icon iconfont icon-less"></i>
+      </div>
+      <div class="swipe-up-content">
+        <p>❤ 向上滑动 ❤</p>
+      </div>
     </div>
 </template>
 
@@ -11,7 +16,24 @@ export default {
 }
 </script>
 
-
-<style scoped>
-
+<style scoped lang="stylus">
+  @keyframes upDown
+    0%
+      bottom 0
+    50%
+      bottom 10px
+    100%
+      bottom 0
+  .icon
+    font-size 40px
+    color #fff
+  .swipe-up-content
+    p
+      margin 0
+      font-size 30px
+  .swipe-up-icon-box
+    opacity .8
+    width 100%
+    text-align center
+    animation upDown 1s ease-in-out infinite
 </style>
--- src/pages/ShowPhotoPage/foreverLove.vue
+++ src/pages/ShowPhotoPage/foreverLove.vue
@@ -1,20 +1,22 @@
 <template>
     <div>
       <div id="fullPage">
-        <div class="section">123</div>
+        <div class="section">123 <swipe-up-icon class="icon"/></div>
         <div class="section">
           <div class="slide" data-anchor="slide1"> 幻灯片 1 </div>
           <div class="slide" data-anchor="slide2"> 幻灯片 2 </div>
           <div class="slide" data-anchor="slide3"> 幻灯片 3 </div>
           <div class="slide" data-anchor="slide4"> 幻灯片 4 </div>
+          <swipe-up-icon class="icon"/>
         </div>
-        <div class="section">123</div>
-        <div class="section">123</div>
+        <div class="section">123 <swipe-up-icon class="icon"/></div>
+        <div class="section">123 <swipe-up-icon class="icon"/></div>
       </div>
     </div>
 </template>
 
 <script>
+  import swipeUpIcon from '../../components/SwipeUpIcon'
   export default {
     data: () => ({
       options: {
@@ -29,10 +31,17 @@
     mounted () {
       // $('#fullPage').fullpage().destroy('all');
       $('#fullPage').fullpage(this.options);
+    },
+    components: {
+      swipeUpIcon
     }
   }
 </script>
 
 <style scoped lang="stylus">
-
+  .icon
+    position absolute
+    bottom 0
+  .section
+    position relative
 </style>
